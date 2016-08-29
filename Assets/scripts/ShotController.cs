4a063ffbfd2209dbc13d9ee918db5bc179050083
fultonbot <steveafulton@gmail.com>
2016-08-29 05:43:11 -0700

    Shot Added

--- Assets/scripts/ShotController.cs
+++ Assets/scripts/ShotController.cs
@@ -1,23 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy1Controller : MonoBehaviour {
+public class ShotController : MonoBehaviour {
+
 	public float speed = 0;
 	// Use this for initialization
 	void Start () {
-		speed = 1.5f;
-	
+		speed = 4.0f;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(0, 0, 100*Time.deltaTime);
-		transform.position += Vector3.down * speed * Time.deltaTime;
+		transform.Rotate(0, 10, 200* -Time.deltaTime);
+		transform.position += Vector3.up * speed * Time.deltaTime;
 		Vector3 exactPos = Camera.main.WorldToScreenPoint (transform.position);
-		Debug.Log(exactPos.x + "," + exactPos.y + "\n");
-
-
-
 
 	}
 }
